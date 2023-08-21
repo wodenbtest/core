0f73c07ef645faf63dae64b4a642e2697d984984
gitaspick <131325146+gitaspick@users.noreply.github.com>
2023-08-21 11:04:03 +0800

    update

--- .github/Examples/reentrancy-error.sol
+++ .github/Examples/reentrancy-error.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.8.21;
 
-
-contract B {
+contract A {
         
     mapping(address => uint) private shares;
 
     function b() external {
         uint amount = shares[msg.sender];
-        msg.sender.transfer(amount);
-        shares[msg.sender] = 0;
-        }            
+        bool a = msg.sender.send(amount);
+        if (a) { shares[msg.sender] = 0; }
+    }
+            
 }
